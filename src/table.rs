c82e62e9597ee41e1a8b08d1929d8f4ac38a2124
Bill Fraser <wfraser@codewise.org>
2020-04-16 00:49:44 -0700

    add tests for table

--- src/table.rs
+++ src/table.rs
@@ -1,16 +1,14 @@
+use std::io::{self, Write};
+
 #[derive(Debug, Copy, Clone)]
 pub enum Alignment {
     Left,
     Right,
 }
 
-impl Default for Alignment {
-    fn default() -> Self {
-        Self::Left
-    }
-}
-
-pub fn print_table(columns: &[Alignment], rows: &[Vec<String>]) {
+pub fn write_table(mut out: impl Write, columns: &[Alignment], rows: &[Vec<String>])
+    -> io::Result<()>
+{
     let mut widths = vec![];
     for i in 0 .. columns.len() {
         let max = rows.iter()
@@ -31,10 +29,81 @@ pub fn print_table(columns: &[Alignment], rows: &[Vec<String>]) {
                 .zip(columns.iter())
                 .zip(widths.iter()) {
             match align {
-                Alignment::Left => print!("{:width$} ", field, width = width),
-                Alignment::Right => print!("{:>width$} ", field, width = width),
+                Alignment::Left => write!(out, "{:width$} ", field, width = width)?,
+                Alignment::Right => write!(out, "{:>width$} ", field, width = width)?,
             }
         }
-        println!();
+        writeln!(out)?;
+    }
+
+    Ok(())
+}
+
+pub fn print_table(columns: &[Alignment], rows: &[Vec<String>]) {
+    write_table(io::stdout(), columns, rows)
+        .expect("I/O error writing to stdout")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::Cursor;
+    use Alignment::{Left, Right};
+
+    fn v(input: &[&[&str]]) -> Vec<Vec<String>> {
+        let mut out = vec![];
+        for row in input {
+            let mut rowvec = vec![];
+            for item in *row {
+                rowvec.push((*item).to_owned());
+            }
+            out.push(rowvec);
+        }
+        out
+    }
+
+    #[test]
+    fn table_test() {
+        let mut out = vec![];
+        write_table(
+            Cursor::new(&mut out),
+            &[Left, Right, Left],
+            &v(&[
+                &["ab",   "cd",   "ef"],
+                &["ghi",  "jkl",  "mno"],
+                &["pqrs", "tuvw", "xyz!"],
+                &["test", "",     "pass"],
+            ]),
+        ).unwrap();
+        let s = String::from_utf8(out).unwrap();
+        assert_eq!(s,
+            "ab     cd ef   \n\
+             ghi   jkl mno  \n\
+             pqrs tuvw xyz! \n\
+             test      pass \n");
+    }
+
+    #[test]
+    #[should_panic(expected = "wrong number of columns in row")]
+    fn test_too_many_columns() {
+        write_table(
+            io::sink(),
+            &[Left, Left, Left],
+            &vec![
+                vec!["foo".into(), "bar".into()],
+                vec!["a".into(), "b".into()],
+            ]).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "wrong number of columns in row")]
+    fn test_not_enough_columns() {
+        write_table(
+            io::sink(),
+            &[Left],
+            &vec![
+                vec!["foo".into(), "bar".into()],
+                vec!["a".into(), "b".into()],
+            ]).unwrap();
     }
 }
